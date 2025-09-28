e00510b4e7db8a1f88497dc90b99e3d605853474
its-diegollull <diego.llull@grupodigitalbank.com>
2025-09-28 20:15:09 -0300

    feat: implement CRUD operations for PrendaBodega with form handling and validation

--- inventario/views.py
+++ inventario/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Producto, Prenda, Bodega, PrendaBodega, MovimientoInventario
+from django.forms import ModelForm
+from django.db import transaction
+from django.core.exceptions import ValidationError
+
+
+
+
 
 
 # Create your views here.
-
-
 
 def home(request):
     return render(request, 'home.html')
@@ -76,23 +81,49 @@ def eliminar_bodega(request, id):
 
 
 # PrendaBodega CRUD
+
+class PrendaBodegaForm(ModelForm):#Formulario para crear y editar prenda bodega
+    class Meta:
+        model = PrendaBodega
+        fields = ['prenda', 'bodega', 'cantidad']
+
 def lista_prenda_bodega(request):
     # logica para listar prenda bodega
+    prenda_bodegas = (
+        PrendaBodega.objects.selected_related('prenda', 'bodega', 'prenda__producto').all()
+    )
     return render(request, 'prenda_bodega_list.html')
 
 def crear_prenda_bodega(request):
+    if request.method == 'POST':
+        form = PrendaBodegaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_prenda_bodega')
+    else:
+        form = PrendaBodegaForm()
     # logica para crear prenda bodega
-    return render(request, 'prenda_bodega_form.html')
+    return render(request, 'prenda_bodega_form.html', {'form': form})
 
 def editar_prenda_bodega(request, id):
+    pb = get_object_or_404(PrendaBodega, pk=id)
+    if request.method == 'POST':
+        form = PrendaBodegaForm(request.POST, instance=pb)
+        if form.is_valid():
+            form.save() 
+            return redirect('lista_prenda_bodega')
+    else:
+        form = PrendaBodegaForm(instance=pb)
     # logica para editar prenda bodega
     return render(request, 'prenda_bodega_form.html')
 
 def eliminar_prenda_bodega(request, id):
+    pb = get_object_or_404(PrendaBodega, pk=id)
+    pb.delete()
     # logica para eliminar prenda bodega
     return render(request, 'prenda_bodega_list.html')
 
-
+    
 
 # Movimientos CRUD
 def lista_movimientos(request):
@@ -110,3 +141,4 @@ def editar_movimiento(request, id):
 def eliminar_movimiento(request, id):
     # logica para eliminar movimiento
     return render(request, 'movimientos_list.html')
+o
